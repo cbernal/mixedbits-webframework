051b1a240ed1351a6cb2f491b69da7399eedbdbf
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2009-12-30 19:10:32 +0000

    Minor api improvements, and changed fieldName to propertyName to match other naming conventions

--- src/main/scala/net.mixedbits.mongo/MongoProperties.scala
+++ src/main/scala/net.mixedbits.mongo/MongoProperties.scala
@@ -8,7 +8,7 @@ import com.mongodb._
 
 trait JsProperty[T]{
   def resolveObject(start:BasicDBObject,create:Boolean):BasicDBObject =
-    MongoTools.resolveObject(start,create,fieldPath)
+    MongoTools.resolveObject(start,create,propertyPath)
   
   def putUncheckedValue[X](start:BasicDBObject,value:X):X = {
     resolveObject(start,true).put(shortName,value)
@@ -29,18 +29,18 @@ trait JsProperty[T]{
   def readValue(start:BasicDBObject):Option[T] = 
     readUncheckedValue(start)
   
-  protected var explicitFieldName:String = null
-  protected def fieldName(newFieldName:String){
-    explicitFieldName = newFieldName
+  protected var explicitPropertyName:String = null
+  protected def propertyName(newPropertyName:String){
+    explicitPropertyName = newPropertyName
   }
-  def fieldName:String = 
-    if(explicitFieldName == null)
-      Objects.simpleClassName(this)
+  lazy val propertyName:String = 
+    if(explicitPropertyName != null)
+      explicitPropertyName
     else
-      explicitFieldName
+      Objects.simpleClassName(this)
     
-  lazy val pathParts = fieldName.split('.')
-  def fieldPath:Array[String] = pathParts.take(pathParts.length-1).toArray
+  lazy val pathParts = propertyName.split('.')
+  def propertyPath:Array[String] = pathParts.take(pathParts.length-1).toArray
   def shortName:String = pathParts.last
   
   //operators
@@ -54,22 +54,22 @@ trait JsProperty[T]{
 }
 
 class JsArrayProperty[T] extends JsProperty[JsArray[T]]{
-  def ==(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(fieldName,"",value.list)
-  def !=(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(fieldName,"$ne",value.list)  
+  def ==(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value.list)
+  def !=(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value.list)  
 }
 
 class JsStringProperty extends JsProperty[String]{
-  def ==(value:String):MongoConstraint = new MongoPropertyConstraint(fieldName,"",value)
-  def !=(value:String):MongoConstraint = new MongoPropertyConstraint(fieldName,"$ne",value)
+  def ==(value:String):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value)
+  def !=(value:String):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value)
 }
 
 abstract class JsNumberProperty[T] extends JsProperty[T]{
-  def ==(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"",value)
-  def !=(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"$ne",value)
-  def >(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"$gt",value)
-  def >=(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"$gte",value)
-  def <(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"$lt",value)
-  def <=(value:T):MongoConstraint = new MongoPropertyConstraint(fieldName,"$lte",value)
+  def ==(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value)
+  def !=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value)
+  def >(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$gt",value)
+  def >=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$gte",value)
+  def <(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$lt",value)
+  def <=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$lte",value)
 }
 
 class JsIntProperty extends JsNumberProperty[Int]
@@ -77,7 +77,7 @@ class JsDoubleProperty extends JsNumberProperty[Double]
 class JsDateProperty extends JsNumberProperty[Date]
 
 //simple runtime definition of properties
-object JsStringProperty{ def apply(name:String) = new JsStringProperty{fieldName(name)} }
-object JsIntProperty{ def apply(name:String) = new JsIntProperty{fieldName(name)} }
-object JsDoubleProperty{ def apply(name:String) = new JsDoubleProperty{fieldName(name)} }
-object JsDateProperty{ def apply(name:String) = new JsDateProperty{fieldName(name)} }
+object JsStringProperty{ def apply(name:String) = new JsStringProperty{propertyName(name)} }
+object JsIntProperty{ def apply(name:String) = new JsIntProperty{propertyName(name)} }
+object JsDoubleProperty{ def apply(name:String) = new JsDoubleProperty{propertyName(name)} }
+object JsDateProperty{ def apply(name:String) = new JsDateProperty{propertyName(name)} }
