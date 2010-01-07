792349a307b2c720b7e44c60172bee3750b151ca
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-01-07 19:04:17 +0000

    Minor refactoring and new features for gridfs

--- src/main/scala/net.mixedbits.mongo/MongoFileSystem.scala
+++ src/main/scala/net.mixedbits.mongo/MongoFileSystem.scala
@@ -12,17 +12,15 @@ class MongoFileSystem(database:MongoDatabase, name:String){
       Objects.simpleClassName(this)
     else
       name
-  val fs = new gridfs.GridFS(database.getDatabase)
+  lazy val fs = new gridfs.GridFS(database.getDatabase)
   
   def store(folder:String, name:String, source:java.io.File) = storeFile(folder,name,fs.createFile(source))
   def store(folder:String, name:String, source:java.io.InputStream) = storeFile(folder,name,fs.createFile(source))
   
   private def storeFile(folder:String, name:String, inputFile:gridfs.GridFSInputFile){
-    val normalizedFolderName = if(folder endsWith "/") folder.substring(0,folder.length-1) else folder
-    val fileExtension = if(name contains ".") name.substring(name.lastIndexOf(".")+1,name.length) else ""
-    inputFile.setFilename(normalizedFolderName+"/"+name)
-    inputFile.getMetaData.put("folder",normalizedFolderName)
-    inputFile.getMetaData.put("extension",fileExtension)
+    inputFile.setFilename(combinePaths(folder,name))
+    inputFile.getMetaData.put("folder",normalizeFolderPath(folder))
+    inputFile.getMetaData.put("extension",fileExtension(name))
     inputFile.setContentType("")
     inputFile.save
   }
@@ -36,4 +34,22 @@ class MongoFileSystem(database:MongoDatabase, name:String){
   def listFiles(folder:String,extension:String) = 
     fs.find(new BasicDBObject("metadata",new BasicDBObject("folder",folder).append("extension",extension)))
   
+  def readFile(folder:String, name:String):Option[java.io.InputStream] =
+    Objects.toOption(fs.findOne(combinePaths(folder,name)))
+      .map(_.getInputStream)
+  
+  private def normalizeFolderPath(folder:String) = 
+    if(folder endsWith "/")
+      folder.substring(0,folder.length-1)
+    else
+      folder
+    
+  private def combinePaths(folder:String, name:String) = 
+    normalizeFolderPath(folder)+"/"+name
+
+  private def fileExtension(name:String) = 
+    if(name contains ".")
+      name.substring(name.lastIndexOf(".")+1,name.length)
+    else
+      ""
 }
