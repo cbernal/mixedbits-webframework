84ddb6d5249fdd782bf87b7f25ce477963b1f4a5
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-01-07 19:05:37 +0000

    New apis for updating and various tweaks

--- trunk/src/main/scala/net.mixedbits.mongo/MongoProperties.scala
+++ trunk/src/main/scala/net.mixedbits.mongo/MongoProperties.scala
@@ -7,6 +7,7 @@ import net.mixedbits.tools.BlockStatements._
 import com.mongodb._
 
 trait JsProperty[T]{
+  //data reading / writing
   def resolveObject(start:BasicDBObject,create:Boolean):BasicDBObject =
     MongoTools.resolveObject(start,create,propertyPath)
   
@@ -29,6 +30,7 @@ trait JsProperty[T]{
   def readValue(start:BasicDBObject):Option[T] = 
     readUncheckedValue(start)
   
+  //property name / parts
   protected var explicitPropertyName:String = null
   protected def propertyName(newPropertyName:String){
     explicitPropertyName = newPropertyName
@@ -43,40 +45,57 @@ trait JsProperty[T]{
   def propertyPath:Array[String] = pathParts.take(pathParts.length-1).toArray
   def shortName:String = pathParts.last
   
-  //operators
-  def equals(value:T):MongoConstraint = this == value // this "operator" helps to remove compiler warnings about all of the ==, >=, <= operators
+  //property groups
+  def and(property:JsProperty[_]) = 
+    new JsPropertyGroup(this,property)
   
-  def ==(value:T):MongoConstraint
-  def !=(value:T):MongoConstraint
+  //query operators
+  def equals(value:T):MongoConstraint = this == value // this "operator" helps to remove compiler warnings about all of the ==, >=, <= operators
+  def ==(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value)
+  def !=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value)
   
   //add support for in, exists, dbref
   
+  //update operators
+  def <~ (value:T):MongoUpdate = 
+    new MongoPropertyUpdate(this.propertyName,"$set",value)
+
 }
+
+class JsAnyProperty extends JsProperty[Any]
 
 class JsArrayProperty[T] extends JsProperty[JsArray[T]]{
-  def ==(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value.list)
-  def !=(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value.list)  
+  override def ==(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value.list)
+  override def !=(value:JsArray[T]):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value.list)
+  
+  def +=(value:T):MongoUpdate = new MongoPropertyUpdate(propertyName,"$push",value)
+  def ++=(value:Seq[T]):MongoUpdate = new MongoPropertyUpdate(propertyName,"$pushAll",if(value.isInstanceOf[JsArray[T]]) value else JsArray(value))
+    
+  def -=(value:T):MongoUpdate = new MongoPropertyUpdate(propertyName,"$pull",value)
+  def --=(value:Seq[T]):MongoUpdate = new MongoPropertyUpdate(propertyName,"$pullAll",if(value.isInstanceOf[JsArray[T]]) value else JsArray(value))
 }
 
-class JsStringProperty extends JsProperty[String]{
-  def ==(value:String):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value)
-  def !=(value:String):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value)
-}
+class JsStringProperty extends JsProperty[String]
 
-abstract class JsNumberProperty[T] extends JsProperty[T]{
-  def ==(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"",value)
-  def !=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$ne",value)
+abstract class JsOrderedProperty[T] extends JsProperty[T]{
   def >(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$gt",value)
   def >=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$gte",value)
   def <(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$lt",value)
-  def <=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$lte",value)
+  def <=(value:T):MongoConstraint = new MongoPropertyConstraint(propertyName,"$lte",value)  
+}
+
+abstract class JsNumberProperty[T <: AnyVal] extends JsOrderedProperty[T]{
+  def +=(value:T):MongoUpdate = new MongoPropertyUpdate(propertyName,"$inc",value)
+  //def -=(value:T):MongoUpdate = new MongoPropertyUpdate(propertyName,"$inc",-value)
 }
 
 class JsIntProperty extends JsNumberProperty[Int]
 class JsDoubleProperty extends JsNumberProperty[Double]
-class JsDateProperty extends JsNumberProperty[Date]
+
+class JsDateProperty extends JsOrderedProperty[Date]
 
 //simple runtime definition of properties
+object JsAnyProperty{ def apply(name:String) = new JsAnyProperty{propertyName(name)} }
 object JsStringProperty{ def apply(name:String) = new JsStringProperty{propertyName(name)} }
 object JsIntProperty{ def apply(name:String) = new JsIntProperty{propertyName(name)} }
 object JsDoubleProperty{ def apply(name:String) = new JsDoubleProperty{propertyName(name)} }
