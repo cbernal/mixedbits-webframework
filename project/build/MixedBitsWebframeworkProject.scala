2b9c5792d38b4ff8987528782143c5ceee147e4f
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-01-19 19:35:44 +0000

    Added in new dependency

--- project/build/MixedBitsWebframeworkProject.scala
+++ project/build/MixedBitsWebframeworkProject.scala
@@ -6,5 +6,7 @@ class MixedBitsWebframeworkProject(info: ProjectInfo) extends DefaultWebProject(
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"  // jetty is only need for testing
   val jasper = "org.apache.tomcat" % "jasper" % "6.0.18"
   
+  val scala_time = "org.scala-tools" % "time" % "2.7.4-0.1"
+  
   lazy val refresh = task{jettyRun.run;None} dependsOn(clean)
 }
