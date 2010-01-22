56a44a01dd34d7d3766932e98e2922d8bfe52f96
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-01-22 16:48:35 +0000

    New captcha library

---
+++ trunk/src/main/scala/net.mixedbits.webframework/Recaptcha.scala
@@ -0,0 +1,29 @@
+package net.mixedbits.webframework
+
+import scala.xml._
+
+import net.tanesha.recaptcha._
+
+class Recaptcha(val publicKey:String, val privateKey:String){
+  val challengeFieldName = "recaptcha_challenge_field"
+  val responseFieldName = "recaptcha_response_field"
+  
+  private def captchaBuilder = ReCaptchaFactory.newReCaptcha(publicKey,privateKey,false)
+  
+  private def captchaChecker = {
+    val checker = new ReCaptchaImpl
+    checker.setPrivateKey(privateKey)
+    checker
+  }
+  
+  def generate():Node =
+    Unparsed(captchaBuilder.createRecaptchaHtml(null,null))
+  
+  def isValid():Boolean = {
+    val request = net.mixedbits.webframework.WebRequest.httpRequest
+    isValid(request.getRemoteAddr,request.getParameter(challengeFieldName),request.getParameter(responseFieldName))
+  }
+  
+  def isValid(clientAddress:String,challenge:String,response:String):Boolean = 
+    captchaChecker.checkAnswer(clientAddress, challenge, response).isValid
+}
