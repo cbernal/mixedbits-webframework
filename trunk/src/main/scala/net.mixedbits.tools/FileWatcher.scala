223f60d34b4873fe57a5da91f4bd3d46f71bae17
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-02-03 23:13:18 +0000

    Maybe a cool tool to check for file updates, maybe not :D

---
+++ trunk/src/main/scala/net.mixedbits.tools/FileWatcher.scala
@@ -0,0 +1,39 @@
+package net.mixedbits.tools
+
+
+import java.io.File
+
+class FileWatcher(files: =>Seq[File]){
+  
+  private def cacheModifiedTimes(files:Seq[File]) = 
+    Map(files.map(f=> (f,f.lastModified)):_*)
+    
+  private var oldModifiedTimes = Map[File,Long]() 
+  
+  private def findModifiedFiles(modifiedTimes:Map[File,Long],newFiles:Seq[File]):Seq[File] = {
+    for(
+      file <- newFiles;
+      //check for new or modified files
+      if !(modifiedTimes contains file) ||
+          modifiedTimes(file) < file.lastModified    
+      ) yield file
+  }
+
+  def modifiedFiles():Seq[File] = {
+    val newFiles = files
+    val modifiedTimes = oldModifiedTimes
+    oldModifiedTimes = cacheModifiedTimes(newFiles)
+    findModifiedFiles(modifiedTimes,newFiles)
+  }
+  
+  def hasUpdates() = {
+    val newFiles = files
+    val modifiedTimes = oldModifiedTimes
+    oldModifiedTimes = cacheModifiedTimes(newFiles)  
+    findModifiedFiles(modifiedTimes,newFiles).size > 0
+  }
+  
+  def isUpToDate() = 
+    findModifiedFiles(oldModifiedTimes,files).size == 0
+
+}
