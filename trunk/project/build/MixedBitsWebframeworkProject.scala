3630accd278e5c5fd825d2ceccd2f0bd506c3596
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-02-18 17:23:25 +0000

    Various required dependencies ...

--- trunk/project/build/MixedBitsWebframeworkProject.scala
+++ trunk/project/build/MixedBitsWebframeworkProject.scala
@@ -2,11 +2,34 @@ import sbt._
 
 class MixedBitsWebframeworkProject(info: ProjectInfo) extends DefaultWebProject(info){
   currentProject =>
+
+
+  /******************
+  | dependencies    |
+  ******************/
   
+  //web server  
   val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.14" % "test->default"  // jetty is only need for testing
   val jasper = "org.apache.tomcat" % "jasper" % "6.0.18"
   
+  //multipart file uploading
+  val commons_fileupload = "commons-fileupload" % "commons-fileupload" % "1.2"
+  
+  //smtp library
+  val java_net_repo = "java.net" at "http://download.java.net/maven/2/"
+  val java_mail = "javax.mail" % "mail" % "1.4.2"
+
+  //joda time wrapper
   val scala_time = "org.scala-tools" % "time" % "2.7.4-0.1"
+  
+  //recaptcha library
+  val recaptcha_repo = "taneshanet" at "http://tanesha.net/maven2"
+  val recaptcha4j = "net.tanesha.recaptcha4j" % "recaptcha4j" % "0.0.7"
+  
+
+  /******************
+  | building        |
+  ******************/
   
   lazy val refresh = task{jettyRun.run;None} dependsOn(clean)
 }
