0c3844adbba7dfb644bd3f3c2e80c8263697e3c1
dan.shryock <dan.shryock@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-02-19 20:43:47 +0000

    Improved session handling system, now has reasonably useful base class for building new session stores, and an implmentation providing a session store for mongodb

---
+++ src/main/scala/net.mixedbits.webframework/MemorySessionProvider.scala
@@ -0,0 +1,34 @@
+package net.mixedbits.webframework
+
+import net.mixedbits.tools._
+
+trait MemorySessionProvider[T >: Null] extends WebSessionProvider[T]{
+  
+  val sessionValueKey = "MemorySessionProvider:Value" 
+  
+  def createSession(value:T):String = {
+    val servletSession = WebRequest.httpRequest.getSession;
+    servletSession.setAttribute(sessionValueKey,value)
+    servletSession.getId
+  }
+
+  def destroyCurrentSession(){
+    val servletSession = WebRequest.httpRequest.getSession
+    servletSession.removeAttribute(sessionValueKey)
+    servletSession.invalidate()
+  }
+  
+  def hasCurrentSession() = {
+    val sessionValue = currentSessionValue
+    sessionValue!= null && sessionValue!=""
+  }
+  
+  def currentSessionId():Option[String] = 
+    if(hasCurrentSession)
+      Some(WebRequest.httpRequest.getSession.getId())
+    else
+      None
+  
+  def currentSessionValue():Option[T] = 
+    Objects.toOption(WebRequest.httpRequest.getSession.getAttribute(sessionValueKey).asInstanceOf[T])
+}
