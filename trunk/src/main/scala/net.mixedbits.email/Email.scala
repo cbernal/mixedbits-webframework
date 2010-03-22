5c0a772b743e9136ce89226d0a5bc997cfb0948e
dan.shryock@gmail.com <dan.shryock@gmail.com@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-03-22 21:43:13 +0000

    Reorganized the email stuff

--- trunk/src/main/scala/net.mixedbits.email/Email.scala
+++ trunk/src/main/scala/net.mixedbits.email/Email.scala
@@ -5,47 +5,73 @@ import javax.mail.internet._
 
 import net.mixedbits.tools._
 
-case class PlainTextMessage(to:String,from:String,subject:String,body:String)
-case class SmtpSession(host:Option[String],port:Option[Int],username:Option[String],password:Option[String]){
-  /*
-  def session() = {
-    
-    mail.smtp.host
-    mail.smtp.port
-    
-    mail.smtp.user
-    mail.smtp.auth
+trait SmtpMessage{
+  def mimeMessage():MimeMessage
+}
+
+case class EmailAddress(email:String,name:Option[String]){
+  def this(email:String) = this(email,None)
+  def this(email:String,name:String) = this(email,Some(name))
+  
+  def toInternetAddress:InternetAddress = {
+    name match {
+      case Some(name) => new InternetAddress(email,name)
+      case None => new InternetAddress(email)
+    }
   }
-  */
-  def send(message:PlainTextMessage):PassFail = {
+}
+
+case class FromEmailAddress(sender:EmailAddress,replyTo:Option[EmailAddress]){
+  def this(email:String) = this(new EmailAddress(email),None)
+  def this(email:String,replyTo:EmailAddress) = this(new EmailAddress(email),Some(replyTo))
+  def this(email:String,name:String) = this(new EmailAddress(email,name),None)
+  def this(email:String,name:String,replyTo:EmailAddress) = this(new EmailAddress(email,name),Some(replyTo))
+  
+  def replyToAddress = replyTo getOrElse sender
+}
+
+case class PlainTextMessage(to:EmailAddress,from:FromEmailAddress,subject:String,body:String) extends SmtpMessage{
+  def mimeMessage():MimeMessage = {
+    val mimeMessage = new MimeMessage(null:Session)
+    mimeMessage.setRecipients(Message.RecipientType.TO,Array[Address](to.toInternetAddress))
+    mimeMessage.setReplyTo(Array(from.replyToAddress.toInternetAddress))
+    mimeMessage.setFrom(from.sender.toInternetAddress)
+    mimeMessage.setSubject(subject)
+    mimeMessage.setContent(body, "text/plain")
+    mimeMessage.saveChanges()
+    mimeMessage
+  }
+}
+
+case class SmtpSession(host:Option[String],port:Option[Int],username:Option[String],password:Option[String]){
+  
+  //deals with all variations of username/password/port/ and hostname
+  def connect():Transport = {
+    val properties = new java.util.Properties
+    for(h <- host)
+      properties.put("mail.smtp.host",h)
+    
+    val session = Session.getInstance(properties, null)
+    val transport = session.getTransport("smtp")
+    
+    for(h <- host; p <- port; user <- username; pass <- password; if !transport.isConnected)
+      transport.connect(h,p,user,pass)
+    for(h <- host; user <- username; pass <- password; if !transport.isConnected)
+      transport.connect(h,user,pass)
+    for(user <- username; pass <- password; if !transport.isConnected)
+      transport.connect(user,pass)
+    
+    if(!transport.isConnected)
+      transport.connect()
+    
+    transport
+  }
+  
+  def send(message:SmtpMessage):PassFail = {
     try{
+      val mimeMessage = message.mimeMessage
       
-      val properties = new java.util.Properties
-      for(h <- host)
-        properties.put("mail.smtp.host",h)
-      
-      val session = Session.getInstance(properties, null)
-      val transport = session.getTransport("smtp")
-      
-      for(h <- host; p <- port; user <- username; pass <- password; if !transport.isConnected)
-        transport.connect(h,p,user,pass)
-      for(h <- host; user <- username; pass <- password; if !transport.isConnected)
-        transport.connect(h,user,pass)
-      for(user <- username; pass <- password; if !transport.isConnected)
-        transport.connect(user,pass)
-      
-      if(!transport.isConnected)
-        transport.connect()
-      
-      
-      val mimeMessage = new MimeMessage(session)
-      mimeMessage.setRecipients(Message.RecipientType.TO,Array[Address](new InternetAddress(message.to)))
-      mimeMessage.setReplyTo(Array(new InternetAddress(message.from)))
-      mimeMessage.setFrom(new InternetAddress(message.from))
-      mimeMessage.setSubject(message.subject)
-      mimeMessage.setContent(message.body, "text/plain")
-      
-      mimeMessage.saveChanges()
+      val transport = connect()
       transport.sendMessage(mimeMessage,mimeMessage.getAllRecipients)
       transport.close
       
@@ -58,26 +84,6 @@ case class SmtpSession(host:Option[String],port:Option[Int],username:Option[Stri
 }
 
 object Email{
-  
-  /*
-  def send(server:String,to:String,from:String,subject:String,body:String):PassFail = {
-    try{
-
-      
-      val session = Session.getInstance(properties, null)
-      val message = new MimeMessage(session)
-      message.setRecipients(Message.RecipientType.TO,Array[Address](new InternetAddress(to)))
-      message.setReplyTo(Array(new InternetAddress(from)))
-      message.setFrom(new InternetAddress(from))
-      message.setSubject(subject)
-      message.setContent(body, "text/plain")
-      Transport.send(message)
-      
-      Success
-    }
-    catch{
-      case e => Failure(e)
-    }
-  }
-  */
+  implicit def stringToEmailAddress(s:String) = new EmailAddress(s)
+  implicit def stringToFromEmailAddress(s:String) = new FromEmailAddress(s)
 }
