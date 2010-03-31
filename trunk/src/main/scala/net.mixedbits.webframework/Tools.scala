1d2da78132331add35c470942b788e322f0b3dc5
dan.shryock@gmail.com <dan.shryock@gmail.com@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-03-31 16:45:55 +0000

    Added utility method for building a node buffer

--- trunk/src/main/scala/net.mixedbits.webframework/Tools.scala
+++ trunk/src/main/scala/net.mixedbits.webframework/Tools.scala
@@ -8,6 +8,12 @@ object Tools{
     new NodeBuffer() &+ element
   
   object Elements{
+    def apply(elements:Seq[Node]) = {
+      val buffer = new NodeBuffer
+      for(e <- elements)
+        buffer &+ e
+      buffer
+    }
     def apply(elements:Elements):Elements = elements
     def apply():Elements = new Elements
   }
