8c5ace5741d05a80f364296e72d06d38eb67aa8f
dan.shryock@gmail.com <dan.shryock@gmail.com@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-06-24 20:37:27 +0000

    Changed to be scala 2.8 compatible

--- trunk/src/main/scala/net.mixedbits.tools/Reflection.scala
+++ trunk/src/main/scala/net.mixedbits.tools/Reflection.scala
@@ -1,13 +1,48 @@
 package net.mixedbits.tools
 
-import scala.tools.nsc.util.NameTransformer._
- 
+//import scala.tools.nsc.util.NameTransformer._
 
 //borrowed from http://gist.github.com/raw/110323/e7ce72633bea69abdfd54e437845bbba51e0704a/reflection.scala
 //no license mentioned, so if it turns out to be non-compatibile, I'll just remove this class
 
 object Reflection {
   implicit def anyToReflectionMethods(x: Any) = new ReflectionMethods(x)
+  /*
+  def callingMethod() =
+    Thread.currentThread.getStackTrace.map(_.getMethodName) drop 2 dropWhile{ name => (name contains "$") || (name contains "<init>") } headOption
+  
+  def test = {
+    testCallingMethods
+  }
+  
+  def testCallingMethods = {
+    println(callingMethod)
+    val x = {
+      println("x: "+callingMethod)
+      10
+    }
+    
+    lazy val y = {
+      println("y: "+callingMethod)
+      10
+    }
+    
+    val z = new AnyRef{
+      println("z: "+callingMethod)
+    }
+    
+    def a() = {
+      println("a: "+callingMethod)
+      b()
+    }
+    
+    def b() = {
+      println("b: "+callingMethod)
+    }
+    
+    "x*y: "+(x*y) + " z: "+z.toString+ " a:"+a()
+  }
+  */
 }
 
 object Module{
@@ -48,8 +83,11 @@ class ReflectionMethods(x: Any) {
       .getDeclaredFields
       .toList
       .map(m => decode(m.toString.replaceFirst("^.*\\.", "")))
+      
+  private def decode(s:String) = s
  
-  private def wrapped: AnyRef = x match {
+  private def wrapped: AnyRef = x.asInstanceOf[AnyRef]
+  /*match {
     case x: Byte => byte2Byte(x)
     case x: Short => short2Short(x)
     case x: Char => char2Character(x)
@@ -60,4 +98,5 @@ class ReflectionMethods(x: Any) {
     case x: Boolean => boolean2Boolean(x)
     case _ => x.asInstanceOf[AnyRef]
   }
+  */
 }
