2fb026e2e161a4485ec7be1da65a1c34c1b66726
dan.shryock <dan.shryock@gmail.com>
2010-08-09 19:43:29 +0000

    Added package object

---
+++ src/main/scala/net.mixedbits.tools/package.scala
@@ -0,0 +1,12 @@
+package net.mixedbits
+
+package object tools
+  extends NumbersImports
+  with StringsImports
+  with SequencesImports
+  with DatesImports
+  with FilesImports
+  with ObjectsImports
+  with BlockStatementsImports
+  with CloseableResourceImports
+  with XmlImports
