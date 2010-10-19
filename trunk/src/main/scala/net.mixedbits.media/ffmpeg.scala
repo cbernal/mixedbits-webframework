e60d87a041f6c7b68ee51ec3b414e59edb99c12e
olas.factor@gmail.com <olas.factor@gmail.com@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-10-19 18:17:21 +0000

    Simplified code :D

--- trunk/src/main/scala/net.mixedbits.media/ffmpeg.scala
+++ trunk/src/main/scala/net.mixedbits.media/ffmpeg.scala
@@ -10,31 +10,21 @@ object ffmpeg extends ((String,InputStream,String,OutputStream,Double=>Unit)=>Un
   def apply(inputParams:String, inputStream:InputStream, outputParams:String, outputStream:OutputStream,progress:Double=>Unit = x => ()){
   
     val process = Runtime.getRuntime().exec("ffmpeg "+inputParams+" -i - "+outputParams+" -")
-    val inputThread = thread{ process.getOutputStream() map {IO.pipeStream(inputStream,_)} }
-    val outputThread = thread{ process.getInputStream() map {IO.pipeStream(_,outputStream)} }
-    val errorThread = thread{
-      for(error <- process.getErrorStream()){
-        
-        val lines = Source.fromInputStream(error).getLines
-        var duration = 1.0
-        for(line <- lines if (line contains "Duration") || (line contains "time="))
-          line match {
-            case _ if line contains "Duration" =>
-              duration =  Time.parseDuration(line.dropWhile(_!=':').drop(1).takeWhile(_!=',').trim)
-            case _ =>
-              progress(min(duration,((line split "\\s+" filter {_ startsWith "time="} head).drop(5)).parseDouble(0)) / duration)
-          }
-
-      }
-    }
+    val threads = List(
+                    thread{ process.getOutputStream() map {IO.pipeStream(inputStream,_)} },
+                    thread{ process.getInputStream() map {IO.pipeStream(_,outputStream)} },
+                    thread{
+                    var duration = 1.0
+                    for(error <- process.getErrorStream();line <- Source.fromInputStream(error).getLines)
+                      if(line contains "Duration")
+                        duration =  Time.parseDuration(line.dropWhile(_!=':').drop(1).takeWhile(_!=',').trim)
+                      else if(line contains "time=")
+                        progress(min(duration,((line split "\\s+" filter {_ startsWith "time="} head).drop(5)).parseDouble(0)) / duration)
+                    })
     
-    val threads = List(inputThread, outputThread, errorThread)
     threads foreach {_.start}
-    val processExitCode = process.waitFor
     threads foreach {_.join}
-    if(processExitCode != 0)
-      error("Exited with error code: "+processExitCode)
-      
-    progress(1)
+    if(process.waitFor != 0)
+      error("Exited with error code: "+process.exitValue)
   }
 }
