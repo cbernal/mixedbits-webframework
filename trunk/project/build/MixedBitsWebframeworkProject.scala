107d4be1cbe1292ff7905a7e1e0d8e691bb312b0
dan.shryock@gmail.com <dan.shryock@gmail.com@f79f8f5e-e5b4-11de-a347-f34a4f72eb7d>
2010-10-20 17:18:24 +0000

    Minor changes to project

--- trunk/project/build/MixedBitsWebframeworkProject.scala
+++ trunk/project/build/MixedBitsWebframeworkProject.scala
@@ -26,7 +26,8 @@ class MixedBitsWebframeworkProject(info: ProjectInfo) extends DefaultProject(inf
     "common-filters" % "common-filters" % "1.0.0M1" from "http://commons-filters.googlecode.com/files/commons-filters-1.0.0M1-dev.jar",//gzip support
     "com.thoughtworks.xstream" % "xstream" % "1.3.1",
     "com.h2database" % "h2" % "1.2.140",
-    "mongodb" % "mongodb" % "2.0" from "http://github.com/downloads/mongodb/mongo-java-driver/mongo-2.0.jar"
+    "org.codehaus.jettison" % "jettison" % "1.2",
+    "mongodb" % "mongodb" % "2.1" from "http://github.com/downloads/mongodb/mongo-java-driver/mongo-2.1.jar"
   )
   
   
