0e7a4f434b47aac7d56fb8fc48a9b10307b6387a
dan.shryock <dan.shryock@gmail.com>
2010-11-19 23:52:51 +0000

    Better error handling

--- src/main/scala/net.mixedbits.media/Time.scala
+++ src/main/scala/net.mixedbits.media/Time.scala
@@ -3,14 +3,18 @@ package net.mixedbits.media
 object Time{
 
   def parseDuration(value:String):Double = {
-    val multipliers = Array(60,60*60,24*60*60)
-    val parts = value split ":"
-    val remainder = parts.reverse drop 1
-    var duration = (parts last) toDouble
-    
-    for((x,multiplier) <- remainder zip multipliers)
-      duration += x.toDouble * multiplier
-    
-    duration
+    try{
+      val multipliers = Array(60,60*60,24*60*60)
+      val parts = value split ":"
+      val remainder = parts.reverse drop 1
+      var duration = (parts last) toDouble
+      
+      for((x,multiplier) <- remainder zip multipliers)
+        duration += x.toDouble * multiplier
+      
+      duration
+    } catch {
+      case e => -1.0
+    }
   }
 }
